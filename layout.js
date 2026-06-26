e8b8d26f6bbfd55f987021d57b89c0bc520cdcd1
joshelorza6-create <joshelorza6@gmail.com>
2026-06-26 00:31:08 -0700

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,12 @@
+﻿export const metadata = {
+  title: "Oso Odds",
+  description: "Oso Odds analytics terminal",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
